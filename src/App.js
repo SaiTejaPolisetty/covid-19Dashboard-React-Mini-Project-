26581b7646d025cc90a9f91d8fb5e3c2616b9c2d
SaiTejaPolisetty <vignanking2@gamil.com>
2022-12-12 07:16:01 +0000

    added not found page and few change to the home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {Route, Switch} from 'react-router-dom'
+
 import Home from './components/Home'
+
+import NotFoundPage from './components/NotFoundPage'
 
 import './App.css'
 
-const App = () => <Home />
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route component={NotFoundPage} />
+  </Switch>
+)
 
 export default App
